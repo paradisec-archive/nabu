5acf44707ba56a37e9b371fdecc3dc8828271ec3
John Ferlito <johnf@inodes.org>
2011-08-08 12:22:48 +1000

    reuse production env in heroku env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,4 @@ Nabu::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  config.action_mailer.delivery_method = :smtp
 end
