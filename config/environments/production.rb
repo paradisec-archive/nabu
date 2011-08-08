b84a6722f26fad4ac8615d8bd6c627fc783feda1
John Ferlito <johnf@inodes.org>
2011-08-08 10:34:32 +1000

    Add heroku email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,4 +49,13 @@ Nabu::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
