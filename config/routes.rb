fe9877924cf909a91e31b038382c3ec29ec759db
John Ferlito <johnf@inodes.org>
2011-12-30 10:26:27 +1100

    Work around heroku/devise/activeadmin precomile issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Nabu::Application.routes.draw do
-  ActiveAdmin.routes(self)
+  # These cause Rails to set up a connection to the production database, which isn't available during
+  # assets:precompile on Heroku, so the following unless block skips setting up these routes only when
+  # rake assets:precompile is being run.
+  #
+  # Could be a problem if the assets needed these to be loaded to compile properly; pretty sure they don't.
+  unless ARGV.join.include? 'assets:precompile'
+    ActiveAdmin.routes(self)
 
-#  devise_for :users, ActiveAdmin::Devise.config
-
-  devise_for :users
+    devise_for :users
+  end
 
   authenticated :user do
     root :to => 'page#dashboard'
