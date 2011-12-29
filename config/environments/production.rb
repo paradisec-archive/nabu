4b4ec2805d9c5b671a84e6d3879cb6bcaea7a4a7
John Ferlito <johnf@inodes.org>
2011-12-30 10:15:42 +1100

    Remove custom precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,6 @@ Nabu::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  # Don't compile partials
-  config.assets.precompile += [ /(^[^_]|\/[^_])[^\/]*/ ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
