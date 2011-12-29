7243451cf48296f370b261e78a4cf3ae9321a391
John Ferlito <johnf@inodes.org>
2011-12-29 16:10:35 +1100

    Add statistics to dashboard

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,5 +3,10 @@ class PageController < ApplicationController
   end
 
   def dashboard
+    @num_collections = Collection.count
+    @num_items = Item.count
+    @num_essences = Essence.count
+    @num_users = User.count
+    @num_universities = University.count
   end
 end
