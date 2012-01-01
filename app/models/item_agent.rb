f5092dc7400ad21292b38d570315422fee1563bb
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2012-01-02 10:31:00 +1100

    Make sure item_agents doesn't get invalid entries.

--- app/models/item_agent.rb
+++ app/models/item_agent.rb
@@ -2,4 +2,10 @@ class ItemAgent < ActiveRecord::Base
   belongs_to :user
   belongs_to :agent_role
   belongs_to :item
+
+  attr_accessible :user_id, :agent_role_id, :item_id
+
+  validates :user_id, :presence => true
+  validates :agent_role_id, :presence => true
+  validates :item_id, :presence => true
 end
