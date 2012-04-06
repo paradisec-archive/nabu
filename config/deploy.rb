f63932ad9e0da81a68d3f662c04f17a78873c532
John Ferlito <johnf@inodes.org>
2012-04-06 16:30:58 +1000

    typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,18 @@ set :ssh_options, {
 
 set :default_shell, "/bin/bash --login"
 
+namespace :app do
+  namespace :solr do
+    desc 'Symlink in-progress deployment to a shared Solr index.'
+    task :symlink, :except => { :no_release => true } do
+      rails_env = fetch(:rails_env, 'production')
+      run "mkdir -p #{release_path}/solr/data && ln -nfs #{shared_path}/solr/data/#{rails_env} #{release_path}/solr/data/#{rails_env}"
+    end
+  end
+end
+
+# Install after hooks for deployment.
+after "deploy:symlink", "app:solr:symlink"
 
 require 'bundler/capistrano'
 require 'capistrano-unicorn'
