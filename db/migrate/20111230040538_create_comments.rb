41b96c4660b74d0ccbb57af1ca149e492e8b9cd5
John Ferlito <johnf@inodes.org>
2012-05-13 16:48:48 +1000

    Fix migration now that we aren;t using opinio

--- db/migrate/20111230040538_create_comments.rb
+++ db/migrate/20111230040538_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.opinio
-
+      t.integer :owner_id,         :null => false
+      t.integer :commentable_id,   :null => false
+      t.string  :commentable_type, :null => false
+      t.text    :body,             :null => false
       t.timestamps
     end
   end
