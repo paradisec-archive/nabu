18e5ae678ac829fbccc340ce9de896290c49d4d1
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2012-06-03 17:52:12 +1000

    Fix typo "Choose an operator" (contributes to #93)

--- app/models/collection_admin.rb
+++ app/models/collection_admin.rb
@@ -2,7 +2,7 @@ class CollectionAdmin < ActiveRecord::Base
   belongs_to :user
   belongs_to :collection
 
-  #validates :collection_id, :presence => true
+  validates :collection_id, :presence => true
   validates :user_id, :presence => true
   validates :collection_id, :uniqueness => {:scope => [:collection_id, :user_id]}
 
