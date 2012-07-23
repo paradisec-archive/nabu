abd1eaa779bebc0516c008a19a642e1d89fe845b
John Ferlito <johnf@inodes.org>
2012-07-23 12:50:04 +1000

    Upgrade to rails 3.2.6

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.2.3'
+gem 'rails', '3.2.6'
 
 # Databases
 gem 'mysql2'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,5 +35,5 @@ Nabu::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  config.action_mailer.default_url_options = { :host => "example.com" }
+  config.action_mailer.default_url_options = { :host => 'example.com' }
 end
