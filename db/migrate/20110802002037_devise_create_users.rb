2aefbaffc6eae46837317580e3835819f05b8b3e
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2012-07-28 21:46:40 +1000

    Fixing email confirmation db fields.

--- db/migrate/20110802002037_devise_create_users.rb
+++ db/migrate/20110802002037_devise_create_users.rb
@@ -26,6 +26,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
 
       ## Lockable
       t.integer  :failed_attempts, :default => 0
