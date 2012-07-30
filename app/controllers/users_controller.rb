b968a83289cc4d676976628945e5a085b1c1db89
John Ferlito <johnf@inodes.org>
2012-07-30 15:12:10 +1000

    move users into active admin (fixes #124)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,6 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
 
-  def index
-    @users = @users.order(sort_column.join(',') + ' ' + sort_direction)
-    params.delete(:search) if params[:clear]
-    if params[:search] || params[:q]
-      match = "%#{params[:search] || params[:q]}%"
-      @users = @users.where{ (first_name =~ match) | (last_name =~ match)  | (address =~ match) | (address2 =~ match) | (country =~ match) | (email =~ match)}
-    end
-
-    if params[:format] != 'csv'
-      @users = @users.page(params[:page]).per(params[:per_page])
-    end
-
-    respond_to do |format|
-      format.html
-      format.csv do
-        fields = [:id, :email, :first_name, :last_name, :address, :address2, :country, :phone, :admin, :rights_transferred_to, :sign_in_count, :last_sign_in_at, :failed_attempts]
-        send_data @users.to_csv(:only => fields), :type => "text/csv; charset=utf-8; header=present"
-      end
-      format.json do
-        render :json => @users.map {|u| {:id => u.id, :name => u.name}}
-      end
-    end
-  end
-
   def show
     render 'users/edit'
   end
@@ -61,16 +37,4 @@ class UsersController < ApplicationController
       render :action => 'edit'
     end
   end
-
-  def destroy
-    # TODO what if the users has collections or is the last user or last admin?
-    begin
-      @user.destroy
-      flash[:notice] = 'User was deleted.'
-    rescue ActiveRecord::DeleteRestrictionError
-      flash[:error] = 'User cannot be deleted - it is in use.'
-    end
-    redirect_to :action => :index
-  end
-
 end
