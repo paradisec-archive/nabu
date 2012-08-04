ef7f3c3060682fd51540e9209f62d0aa24e26866
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2012-08-04 19:20:45 +1000

    Cleanup for file processing dashboard.

--- app/admin/file_processing.rb
+++ app/admin/file_processing.rb
@@ -1,14 +1,14 @@
 ActiveAdmin.register_page "File Processing" do
   content do
     div :class => 'dashboard_section panel' do
-      h3 "Collections with example item not ready for export (max 20)"
+      h3 'Collections with at least one item not ready for export (max 20)'
       div :class => 'panel_contents' do      
         insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, Item.where(:metadata_exportable => false).group(:collection_id).order('id desc').limit(20) do
           column :full_identifier
           column :collection_id do |item|
             link_to item.collection.identifier, "/collections/#{item.collection.id}"
           end
-          column :id do |item|
+          column :sample_item do |item|
             link_to item.identifier, "/items/#{item.id}"
           end
           column :title
@@ -18,7 +18,7 @@ ActiveAdmin.register_page "File Processing" do
     end
 
     div :class => 'dashboard_section panel' do
-      h3 "Recently imported files (max 20)"
+      h3 'Recently imported files (max 20)'
       div :class => 'panel_contents' do
         insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, Essence.order('id desc').limit(20) do
           column :id do |essence|
@@ -39,6 +39,7 @@ ActiveAdmin.register_page "File Processing" do
 
     div :class => 'dashboard_section panel' do
       render 'paths'
+      para 'Note: the machine is set up to check directories once a minute.'
     end    
   end
 end
