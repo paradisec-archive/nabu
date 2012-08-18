1cf9c19c531a95fe82f4e95a393cffe8b9d4ac7f
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2012-08-18 15:47:42 +1000

    Update of ANDS RIF-CS feed.

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,8 +1,13 @@
 class University < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
 
-  attr_accessible :name
+  attr_accessible :name, :party_identifier
 
   scope :alpha, order(:name)
   paginates_per 10
+
+  def full_path
+    # FIX ME
+    "http://catalog.paradisec.org.au/admin/universities/#{identifier}"
+  end
 end
