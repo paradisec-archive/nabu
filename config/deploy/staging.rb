6055e6d5b6d3ff55cbfa644db8924aa93b17f114
John Ferlito <johnf@inodes.org>
2012-09-29 19:24:11 +1000

    Remove staging

--- config/deploy/staging.rb
+++
@@ -1,8 +0,0 @@
-role :web, 'catalog.paradisec.org.au'
-role :app, 'catalog.paradisec.org.au'
-role :db,  'catalog.paradisec.org.au', :primary => true # This is where Rails migrations will run
-
-set :rails_env,   'staging'
-set :unicorn_env, 'staging'
-set :app_env,     'staging'
-set :bundle_without, [:test]
