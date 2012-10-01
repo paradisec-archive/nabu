ea41610b51584e94d26c68ca4bc7c4d99cd27d92
John Ferlito <johnf@inodes.org>
2012-10-01 19:42:34 +1000

    Add activity log to admin panel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120923013322) do
+ActiveRecord::Schema.define(:version => 20121001084308) do
 
   create_table "access_conditions", :force => true do |t|
     t.string   "name"
@@ -303,12 +303,13 @@ ActiveRecord::Schema.define(:version => 20120923013322) do
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
   create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
+    t.string   "item_type",      :null => false
+    t.integer  "item_id",        :null => false
+    t.string   "event",          :null => false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
+    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
