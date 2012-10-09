dd0a45c213d82b40c3ca311d2a11563968fd6bc1
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2012-10-10 09:44:02 +1100

    Require the correct ExSite9

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,4 @@
-require_dependency 'ExSite9'
+require 'ExSite9'
 class CollectionsController < ApplicationController
   load_and_authorize_resource :find_by => :identifier, :except => [:search, :advanced_search, :bulk_update, :bulk_edit]
   authorize_resource :only => [:advanced_search, :bulk_update, :bulk_edit]
