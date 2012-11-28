0fecece71e713c18fab88a53b1e4ef48ff73d7c0
John Ferlito <johnf@inodes.org>
2012-11-27 21:20:25 -0800

    Add Catalog report (closes: #179)

---
+++ db/migrate/20121027032001_add_timestampes_to_essences.rb
@@ -0,0 +1,13 @@
+class AddTimestampesToEssences < ActiveRecord::Migration
+  def change
+    change_table :essences do |t|
+      t.timestamps
+    end
+
+    Essence.reset_column_information
+
+    Item.select(['id', 'created_at', 'updated_at']).find_each do |item|
+      Essence.update_all({:created_at => item.created_at, :updated_at => item.updated_at}, :item_id => item.id)
+    end
+  end
+end
