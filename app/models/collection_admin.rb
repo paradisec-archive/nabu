cbbf90cb583df32362c89a4d7dfa8f9fdb5a0602
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2012-12-20 09:47:15 +1100

    AILS bug - can't save collection_admin without collection_admin having been saved Closes #293

--- app/models/collection_admin.rb
+++ app/models/collection_admin.rb
@@ -2,7 +2,8 @@ class CollectionAdmin < ActiveRecord::Base
   belongs_to :user
   belongs_to :collection
 
-  validates :collection_id, :presence => true
+# RAILS bug - can't save collection_admin without collection_admin having been saved
+#  validates :collection_id, :presence => true
   validates :user_id, :presence => true
   validates :collection_id, :uniqueness => {:scope => [:collection_id, :user_id]}
 
