b68ccc6418772ee9c33803483cf412000e960235
John Ferlito <johnf@inodes.org>
2013-02-16 19:43:11 +1100

    Support Zeus better

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+require 'rspec/autorun' unless defined?(Zeus)
 
 require 'capybara/rspec'
 require 'capybara/poltergeist'
