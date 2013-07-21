c51bfb0e4e01499ba6d08517e1e3a5558cac84a9
John Ferlito <johnf@inodes.org>
2013-07-21 13:46:41 +1000

    Add rollbar deployment notification

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,5 +71,14 @@ namespace :monit do
   before 'deploy:restart', 'monit:unmonitor'
 end
 
+task :notify_rollbar, :roles => :app do
+  set :revision, `git log -n 1 --pretty=format:"%H"`
+  set :local_user, `whoami`
+  set :rollbar_token, capture("cat #{shared_path}/config/rolbar.txt")
+  rails_env = fetch(:rails_env, 'production')
+  run "curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{rollbar_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user} >/dev/null 2>&1", :once => true
+end
+after :deploy, 'notify_rollbar'
+
 require 'bundler/capistrano'
 require 'capistrano-unicorn'
