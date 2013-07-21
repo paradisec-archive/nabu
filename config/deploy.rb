f121aea8ee6f3bbd680ce3d3e0c9e0b3756c4942
John Ferlito <johnf@inodes.org>
2013-07-21 13:56:55 +1000

    Typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,7 +74,7 @@ end
 task :notify_rollbar, :roles => :app do
   set :revision, `git log -n 1 --pretty=format:"%H"`
   set :local_user, `whoami`
-  set :rollbar_token, capture("cat #{shared_path}/config/rolbar.txt")
+  set :rollbar_token, capture("cat #{shared_path}/config/rollbar.txt")
   rails_env = fetch(:rails_env, 'production')
   run "curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{rollbar_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user} >/dev/null 2>&1", :once => true
 end
