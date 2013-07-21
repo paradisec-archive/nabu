de6b54927dc3365557aa7fdfdd42e1c701eb93bb
John Ferlito <johnf@inodes.org>
2013-07-21 16:12:25 +1000

    Comment some code

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -5,15 +5,18 @@ class LanguagesController < ApplicationController
 
   def index
     @languages = @languages.order('languages.name').where('languages.name like ?', "%#{params[:q]}%").limit(10)
+    # TODO Does ths code path even get hit anymore with the split that happens in select2?
     if params[:country_ids]
       country_ids = params[:country_ids].split(/,/)
       # TODO there should be a better way of doing this
       @languages = @languages.where(:countries_languages => {:country_id => country_ids})
     end
 
-    @languages.push(Language.find_by_code("mul"));
-    @languages.push(Language.find_by_code("und"));
-    @languages.push(Language.find_by_code("zxx"));
+    # These are fake languages which we always want in the list
+    @languages << Language.find_by_code('mul')
+    @languages << Language.find_by_code('und')
+    @languages << Language.find_by_code('zxx')
+
     respond_with @languages
   end
 
