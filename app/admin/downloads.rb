156cb8746b031e8c53f9614dd462c0c61e63a07a
John Ferlito <johnf@inodes.org>
2013-09-07 21:21:18 -0700

    Fix Active Admin for Download

--- app/admin/downloads.rb
+++ app/admin/downloads.rb
@@ -2,7 +2,8 @@ ActiveAdmin.register Download do
   sidebar :paginate, :only => :index  do
     para button_tag 'Show 10', :class => 'per_page', :data => {:per => 10}
     para button_tag 'Show 50', :class => 'per_page', :data => {:per => 50}
-    button_tag "Show all #{User.count}", :class => 'per_page', :data => {:per => count}
+    count = Download.count
+    button_tag "Show all #{count}", :class => 'per_page', :data => {:per => count}
   end
 
   # change pagination
@@ -11,28 +12,43 @@ ActiveAdmin.register Download do
   end
 
   # index page search sidebar
-  filter :user
-  filter :essence
+  filter :user_first_name, :as => :string
+  filter :user_last_name, :as => :string
+  filter :essence_item_identifier, :as => :string, :label => 'Item Identifier'
+  filter :essence_item_collection_identifier, :as => :string, :label => 'Collection Identifier'
+  filter :created_at
 
   # index page
   index do
     id_column
     column :user
-    column :essence
-    column :item
-    column :collection
+    column :essence do |download|
+      link_to download.essence.full_identifier, Rails.application.routes.url_helpers.collection_item_essence_path(download.collection, download.item, download.essence)
+    end
+    column :item do |download|
+      link_to download.item.full_identifier, Rails.application.routes.url_helpers.collection_item_path(download.collection, download.item)
+    end
+    column :collection do |download|
+      link_to download.collection.identifier, Rails.application.routes.url_helpers.collection_path(download.collection)
+    end
     column :created_at
     default_actions
   end
 
   # show page
-  show do |user|
+  show do |download|
     attributes_table do
       row :id
       row :user
-      row :essence
-      row :item
-      row :collection
+      row :essence do
+        link_to download.essence.full_identifier, Rails.application.routes.url_helpers.collection_item_essence_path(download.collection, download.item, download.essence)
+      end
+      row :item do
+        link_to download.item.full_identifier, Rails.application.routes.url_helpers.collection_item_path(download.collection, download.item)
+      end
+      row :collection do
+        link_to download.collection.identifier, Rails.application.routes.url_helpers.collection_path(download.collection)
+      end
       row :created_at
     end
   end
