b51b6cf7235166bf1760609a09153d4e60e3f2ba
Jonathon Gillett <jonathon.gillett@terem.com.au>
2015-04-07 16:56:47 +1000

    trying to fix migrations

--- db/migrate/20150311065243_change_funding_body_to_multiple.rb
+++ db/migrate/20150311065243_change_funding_body_to_multiple.rb
@@ -1,27 +1,23 @@
 class ChangeFundingBodyToMultiple < ActiveRecord::Migration
   def up
-    create_table :collections_funding_bodies, :id => false do |t|
-      t.references :collection, :null => false
-      t.references :funding_body, :null => false
-    end
-    add_index :collections_funding_bodies, [:collection_id, :funding_body_id], name: :lookup_by_collection_and_funding_body_index
-
     Collection.class_eval do
       belongs_to :old_funding_body, class_name: 'FundingBody', foreign_key: 'funding_body_id'
     end
 
     Collection.all.each do | coll |
       unless coll.old_funding_body.nil?
-        coll.funding_bodies << coll.old_funding_body
+        coll.grants << Grant.new({collection_id: coll.id, funding_body_id: coll.old_funding_body.id, grant_identifier: coll.grant_identifier})
         coll.save
       end
     end
 
     remove_column :collections, :funding_body_id
+    remove_column :collections, :grant_identifier
   end
 
   def down
     add_column :collections, :funding_body_id, :integer
+    add_column :collections, :grant_identifier, :string
 
     Collection.class_eval do
       belongs_to :new_funding_body, class_name: 'FundingBody', foreign_key: 'funding_body_id'
@@ -29,12 +25,11 @@ class ChangeFundingBodyToMultiple < ActiveRecord::Migration
 
     Collection.all.each do | coll |
       # Note: this will drop all but the first funding body, if multiple are present
-      unless coll.funding_bodies.empty?
-        coll.new_funding_body = coll.funding_bodies.first
+      unless coll.grants.empty?
+        coll.new_funding_body = coll.grants.first.funding_body
+        coll.grant_identifier = coll.grants.first.grant_identifier
         coll.save
       end
     end
-
-    drop_table :collections_funding_bodies
   end
 end
