695d9fbce8e9f6f990683368fb7b8f98578bb89b
Stas Belkov <belkiii@gmail.com>
2015-05-12 11:41:41 +0800

    Issue #423: Add access headers to the OAI controller actions to enable CORS.

--- app/controllers/oai_controller.rb
+++ app/controllers/oai_controller.rb
@@ -1,4 +1,6 @@
 class OaiController < ApplicationController
+  before_filter :set_access_headers
+
   def item
     # Remove controller and action from the options.  Rails adds them automatically.
     options = params.delete_if { |k,v| %w{controller action}.include?(k) }
@@ -13,5 +15,14 @@ class OaiController < ApplicationController
     provider = CollectionProvider.new
     response =  provider.process_request(options)
     render :text => response, :content_type => 'text/xml'
+  end
+
+  private
+
+  def set_access_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
