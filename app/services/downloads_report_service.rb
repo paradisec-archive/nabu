b84fb17eee13baa2f54c6a464c0c23062660c298
Jonathon Gillett <jonathon.gillett@terem.com.au>
2015-07-15 11:52:09 +1000

    Fixes #410 lowercase all the table names to match the database and standard procedure

--- app/services/downloads_report_service.rb
+++ app/services/downloads_report_service.rb
@@ -31,22 +31,22 @@ class DownloadsReportService
     active_date_from = @date_from - 1.day
     active_date_to = @date_to + 1.day
 
-    "SELECT Downloads.created_at,
-        Users.country,
-        Collections.title AS collection_title,
-        Essences.filename,
-        Items.title AS item_title
-      FROM Downloads
-        INNER JOIN Essences ON
-        Downloads.essence_id = Essences.id
-        INNER JOIN Items ON
-        Essences.item_id = Items.id
-        INNER JOIN Collections ON
-        Items.collection_id = Collections.id
-        INNER JOIN Users ON
-        Downloads.user_id = Users.id
-      WHERE Items.collector_id = #{@user.id}
-      AND Downloads.created_at BETWEEN '#{active_date_from.strftime('%Y-%m-%d %H:%M:%S')}' AND '#{active_date_to.strftime('%Y-%m-%d %H:%M:%S')}'
+    "SELECT downloads.created_at,
+        users.country,
+        collections.title AS collection_title,
+        essences.filename,
+        items.title AS item_title
+      FROM downloads
+        INNER JOIN essences ON
+          downloads.essence_id = essences.id
+        INNER JOIN items ON
+          essences.item_id = items.id
+        INNER JOIN collections ON
+          items.collection_id = collections.id
+        INNER JOIN users ON
+          downloads.user_id = users.id
+      WHERE items.collector_id = #{@user.id}
+      AND downloads.created_at BETWEEN '#{active_date_from.strftime('%Y-%m-%d %H:%M:%S')}' AND '#{active_date_to.strftime('%Y-%m-%d %H:%M:%S')}'
      "
   end
 end
