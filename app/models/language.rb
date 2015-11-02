b564c8acbca9998c0f3ffd6d58d948a68575762f
Andrew Grimm <andrew.grimm@terem.com.au>
2015-11-02 11:08:25 +1100

    Issue 488 WIP: Fix has_many relationships

--- app/models/language.rb
+++ app/models/language.rb
@@ -30,10 +30,10 @@ class Language < ActiveRecord::Base
   #validates :countries, :length => { :minimum => 1 }
 
   has_many :item_content_languages
-  has_many :items, :through => :item_content_languages, :dependent => :restrict
+  has_many :items_for_content, :through => :item_content_languages, :source => :item, :dependent => :restrict
 
   has_many :item_subject_languages
-  has_many :items, :through => :item_subject_languages, :dependent => :restrict
+  has_many :items_for_subject, :through => :item_subject_languages, :source => :item, :dependent => :restrict
 
   has_many :collection_languages
   has_many :collections, :through => :collection_languages, :dependent => :restrict
