be5e93cbbd4cf343ca5bc7524eae2b70352eef83
Andrew Grimm <andrew.grimm@terem.com.au>
2016-01-25 17:54:42 +1100

    Fix #512 Language filters have been changed and are now wrong

--- app/admin/countries.rb
+++ app/admin/countries.rb
@@ -2,4 +2,8 @@ ActiveAdmin.register Country do
   menu :parent => "Other Entities"
   config.sort_order = "name"
   actions :all, :except => [:destroy]
+
+  # Don't filter by countries_languages, languages, collection_languages, collections, or latlon boundaries
+  filter :code
+  filter :name
 end
