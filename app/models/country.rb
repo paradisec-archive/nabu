043ffb8e6c705d10f3f34f516d24bf3605dc265e
Andrew Grimm <andrew.grimm@terem.com.au>
2016-03-09 09:36:59 +1100

    Issue #490 WIP: Delete nonsensical has_many. Result of copypasta which was partially fixed in issue #407

--- app/models/country.rb
+++ app/models/country.rb
@@ -21,8 +21,5 @@ class Country < ActiveRecord::Base
   has_many :countries_languages
   has_many :languages, :through => :countries_languages, :dependent => :restrict
 
-  has_many :collection_languages
-  has_many :collections, :through => :collection_languages, :dependent => :restrict
-
   has_one :latlon_boundary
 end
