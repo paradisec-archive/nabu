6a997d2a754f4e93675caabd73e66da3b00a98f7
Andrew Grimm <andrew.grimm@terem.com.au>
2016-07-18 13:29:58 +1000

    Issue #592 WIP: Annotate

--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: data_types
+#
+#  id   :integer          not null, primary key
+#  name :string(255)      not null
+#
+
 class DataType < ActiveRecord::Base
   attr_accessible :name
 end
