201fce20b6b874b318aaece62d9796c066585d21
Andrew Grimm <andrew.grimm@terem.com.au>
2016-07-20 13:12:11 +1000

    Issue #592 WIP: Resolve overlooked change

--- app/models/item.rb
+++ app/models/item.rb
@@ -131,9 +131,8 @@ class Item < ActiveRecord::Base
   delegate :name, :to => :discourse_type, :prefix => true, :allow_nil => true
   delegate :name, :to => :access_condition, :prefix => true, :allow_nil => true
 
-  # WIP: Need to include data_types
   DUPLICATABLE_ASSOCIATIONS = %w(countries subject_languages content_languages
-                         admins users agents data_categories)
+                         admins users agents data_categories data_types)
 
 
   paginates_per 10
