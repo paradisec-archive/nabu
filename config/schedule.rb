9d84293a105a038a9f9bbc8a163c80df8554398f
Stas Belkov <stanislav.belkov@terem.com.au>
2016-11-09 14:15:56 +1100

    Remove mint_dois task from schedule.rb, as it was causing errors.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
 # Use this file to easily define all of your cron jobs.
 
-every 1.day, :at => '4:30 am' do
-  rake "archive:mint_dois"
-end
+# every 1.day, :at => '4:30 am' do
+#   rake "archive:mint_dois"
+# end
 
 every 1.day, :at => '4:30 am' do
   rake "data:check_all_checksums"
