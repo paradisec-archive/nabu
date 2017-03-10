06bbeb759f5a81ebbf24d611b87f22b0c5f810e2
Jonathon Gillett <jonathon.gillett@terem.com.au>
2017-03-10 13:11:29 +1100

    replace avprobe with ffprobe for better media inspection. make it silent to avoid logspam

--- lib/media.rb
+++ lib/media.rb
@@ -106,10 +106,10 @@ module Nabu
     def probe
       return @data if @data
 
-      output = %x{avprobe -show_format -show_streams -of json #@file}
-      raise "Error running avprobe, returned #{$?} output: #{output}" unless $?.success?
+      output = %x{ffprobe -v0 -show_format -show_streams -of json #{@file}}
+      raise "Error running ffprobe, returned #{$?} output: #{output}" unless $?.success?
 
-      # deal with invlaid UTF-8
+      # deal with invalid UTF-8
       output.encode!('UTF-8', 'UTF-8', :invalid => :replace)
 
       @data = JSON.parse output
