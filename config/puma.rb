01648fb9d858ca209dd7e51e0d5ccc85e2cb31c7
John Ferlito <johnf@inodes.org>
2022-12-15 21:01:19 +1100

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -22,7 +22,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/puma.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
