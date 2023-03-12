16fb765212a8bb0e036434ed73d9820b159e7212
John Ferlito <johnf@inodes.org>
2023-03-12 12:32:34 +1100

    Adjust sentry sampling

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,8 +6,4 @@ Sentry.init do |config|
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
 end
