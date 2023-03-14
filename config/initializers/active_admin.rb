ae32390bbb75b67b3ae69e108c53039fac576e0c
John Ferlito <johnf@inodes.org>
2023-03-14 21:45:47 +1100

    Fix sentry

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -151,7 +151,6 @@ ActiveAdmin.setup do |config|
   # Active Admin resources and pages from here.
   #
   # config.before_action :do_something_awesome
-  config.before_action = :set_sentry_user
 
   # == Attribute Filters
   #
