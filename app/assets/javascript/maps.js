879731176cd9ce2c843cda955e76a05bb0a44330
John Ferlito <johnf@inodes.org>
2023-03-19 21:58:24 +1100

    Fix marker clustering

--- app/assets/javascript/maps.js
+++ app/assets/javascript/maps.js
@@ -56,21 +56,21 @@ const initMap = () => {
     set_map_bounds_from_ajax('/languages/', language_ids);
   });
 
-  document.querySelector('.map').bind?.('update_map', (event) => {
-    const north_limit = document.querySelector('.north_limit').value || event.currentTarget.dataset.northLimit || 80;
-    const south_limit = document.querySelector('.south_limit').value || event.currentTarget.dataset.southLimit || -80;
-    const east_limit = document.querySelector('.east_limit').value || event.currentTarget.dataset.eastLimit || -40;
-    const west_limit = document.querySelector('.west_limit').value || event.currentTarget.dataset.westLimit || -20;
-    const editable = event.currentTarget.data.editable === true;
+  document.querySelector('.map').addEventListener('update_map', (event) => {
+    const north_limit = document.querySelector('.north_limit')?.value || event.target.dataset.northLimit || 80;
+    const south_limit = document.querySelector('.south_limit')?.value || event.target.dataset.southLimit || -80;
+    const east_limit = document.querySelector('.east_limit')?.value || event.target.dataset.eastLimit || -40;
+    const west_limit = document.querySelector('.west_limit')?.value || event.target.dataset.westLimit || -20;
+    const editable = event.target.dataset.editable === true;
 
     const sw = new google.maps.LatLng(south_limit, west_limit);
     const ne = new google.maps.LatLng(north_limit, east_limit);
     const bounds = new google.maps.LatLngBounds(sw, ne);
 
-    const map = event.currentTarget.dataset.map;
+    const map = event.target.map;
     map.fitBounds(bounds);
 
-    let rect = event.currentTarget.dataset.rect;
+    let rect = event.target.rect;
     if (rect) {
       rect.setBounds(bounds)
     } else {
@@ -79,7 +79,7 @@ const initMap = () => {
         editable: editable,
         map: map
       });
-      event.currentTarget.dataset.rect = rect;
+      event.target.rect = rect;
     }
 
     google.maps.event.addListener(rect, 'bounds_changed', () => { // eslint-disable-line no-undef
@@ -108,7 +108,7 @@ const initMap = () => {
     }
 
     const map = new google.maps.Map(element, options); // eslint-disable-line no-undef
-    element.dataset.map = map;
+    element.map = map;
     element.dispatchEvent(new Event('update_map'))
   });
 
@@ -128,7 +128,7 @@ const initMap = () => {
     };
 
     const map = new google.maps.Map(element, options);
-    element.dataset.map = map;
+    element.map = map;
 
     const north_limit = element.dataset.northLimit;
     const south_limit = element.dataset.southLimit;
