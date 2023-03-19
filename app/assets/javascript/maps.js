b62853891e70e25e1960097501012f353c0ce417
John Ferlito <johnf@inodes.org>
2023-03-19 21:16:18 +1100

    Remove jquery from maps.js

--- app/assets/javascript/maps.js
+++ app/assets/javascript/maps.js
@@ -1,36 +1,25 @@
 const initMap = () => {
   const set_map_bounds_from_ajax = (path, ids) => {
-    let north_limit = null;
-    let south_limit = null;
-    let east_limit = null;
-    let west_limit = null;
+    const marker_bounds = new google.maps.LatLngBounds();
 
-    const marker_bounds = new google.maps.LatLngBounds(); // eslint-disable-line no-undef
-
-    // const map = $('.map').data('map');
-
-    ids.forEach((id) => {
-      let data = null;
-      $.ajax({
-        url: path + id + "?location_only=true",
-        dataType: 'json',
-        async: false,
-        success: (object) => {
-          data = object
-        }
-      });
+    ids.forEach(async (id) => {
+      // TOOD: Do we have to set data type as json?
+      const data = await fetch(`${path}${id}?location_only=true`);
 
       if (!data || !data['north_limit']) {
+        console.debug('NO data', data);
         return;
       }
 
-      north_limit = data['north_limit'];
-      south_limit = data['south_limit'];
-      east_limit = data['east_limit'];
-      west_limit = data['west_limit'];
+      const {
+        north_limit,
+        south_limit,
+        east_limit,
+        west_limit,
+      } = data;
 
-      const sw = new google.maps.LatLng(south_limit, west_limit); // eslint-disable-line no-undef
-      const ne = new google.maps.LatLng(north_limit, east_limit); // eslint-disable-line no-undef
+      const sw = new google.maps.LatLng(south_limit, west_limit);
+      const ne = new google.maps.LatLng(north_limit, east_limit);
       marker_bounds.extend(sw);
       marker_bounds.extend(ne);
     });
@@ -39,82 +28,75 @@ const initMap = () => {
     const sw = marker_bounds.getSouthWest();
 
     if (marker_bounds.isEmpty()) {
-      displayNoDataMessage(path);
-    } else {
-      $('.north_limit').val(ne.lat());
-      $('.east_limit').val(ne.lng());
-      $('.south_limit').val(sw.lat());
-      $('.west_limit').val(sw.lng());
+      const node = document.querySelector('.no-map-match-message');
+      node.textContent = `No matching map data found from ${path == '/countries/' ? 'country' : 'language'}`;
 
-      clearNoDataMessage();
-      $('.map').trigger('update_map');
+      return;
     }
+
+    document.querySelector('.north_limit').value = ne.lat();
+    document.querySelector('.east_limit').value = ne.lng();
+    document.querySelector('.south_limit').value = sw.lat();
+    document.querySelector('.west_limit').value = sw.lng();
+
+    document.querySelector('.no-map-match-message').textContent = '';
+
+    document.querySelector('.map').dispatchEvent(new Event('update_map'));
 
     return false;
   }
 
-  const displayNoDataMessage = (path) => {
-    if (path == '/countries/') {
-      $('.no-map-match-message').text('No matching map data found from country')
-    } else {
-      $('.no-map-match-message').text('No matching map data found from language')
-    }
-  };
-
-  const clearNoDataMessage = () => {
-    $('.no-map-match-message').text('')
-  };
-
-  $('#set-map-from-country').on('click', function() {
-    const country_ids = $('.country').val().split(/,/);
+  document.querySelector('#set-map-from-country')?.addEventListener('click', function() {
+    const country_ids = document.querySelector('.country').value.split(/,/);
     set_map_bounds_from_ajax('/countries/', country_ids);
   });
 
-  $('#set-map-from-language').on('click', function() {
-    const language_ids = $('.language').val().split(/,/)
+  document.querySelector('#set-map-from-language')?.addEventListener('click', function() {
+    const language_ids = document.querySelector('.language').value.split(/,/)
     set_map_bounds_from_ajax('/languages/', language_ids);
   });
 
-  $('.map').bind('update_map', (event) => {
-    const north_limit = $('.north_limit').val() || $(event.currentTarget).data('north-limit') || 80;
-    const south_limit = $('.south_limit').val() || $(event.currentTarget).data('south-limit') || -80;
-    const east_limit = $('.east_limit').val() || $(event.currentTarget).data('east-limit') || -40;
-    const west_limit = $('.west_limit').val() || $(event.currentTarget).data('west-limit') || -20;
-    const editable = $(event.currentTarget).data('editable') == true;
+  document.querySelector('.map')?.bind('update_map', (event) => {
+    const north_limit = document.querySelector('.north_limit').value || event.currentTarget.dataset.northLimit || 80;
+    const south_limit = document.querySelector('.south_limit').value || event.currentTarget.dataset.southLimit || -80;
+    const east_limit = document.querySelector('.east_limit').value || event.currentTarget.dataset.eastLimit || -40;
+    const west_limit = document.querySelector('.west_limit').value || event.currentTarget.dataset.westLimit || -20;
+    const editable = event.currentTarget.data.editable === true;
 
-    const sw = new google.maps.LatLng(south_limit, west_limit); // eslint-disable-line no-undef
-    const ne = new google.maps.LatLng(north_limit, east_limit); // eslint-disable-line no-undef
-    const bounds = new google.maps.LatLngBounds(sw, ne); // eslint-disable-line no-undef
+    const sw = new google.maps.LatLng(south_limit, west_limit);
+    const ne = new google.maps.LatLng(north_limit, east_limit);
+    const bounds = new google.maps.LatLngBounds(sw, ne);
 
-    const map = $(event.currentTarget).data('map');
+    const map = event.currentTarget.dataset.map;
     map.fitBounds(bounds);
 
-    let rect = $(event.currentTarget).data('rect');
+    let rect = event.currentTarget.dataset.rect;
     if (rect) {
       rect.setBounds(bounds)
     } else {
-      rect = new google.maps.Rectangle({ // eslint-disable-line no-undef
+      rect = new google.maps.Rectangle({
         bounds: bounds,
         editable: editable,
         map: map
       });
-      $(event.currentTarget).data('rect', rect);
+      event.currentTarget.dataset.rect = rect;
     }
 
     google.maps.event.addListener(rect, 'bounds_changed', () => { // eslint-disable-line no-undef
       const bounds = rect.getBounds();
       const ne = bounds.getNorthEast();
       const sw = bounds.getSouthWest();
-      $('.north_limit').val(ne.lat());
-      $('.south_limit').val(sw.lat());
-      $('.east_limit').val(ne.lng());
-      $('.west_limit').val(sw.lng());
+      document.querySelector('.north_limit').value = ne.lat();
+      document.querySelector('.south_limit').value = sw.lat();
+      document.querySelector('.east_limit').value = ne.lng();
+      document.querySelector('.west_limit').value = sw.lng();
     })
   });
 
-  $('.map').each((_, element) => {
-    const cw = $(element).width();
-    $(element).css({ 'height': cw + 'px', 'max-height': '400px' })
+  document.querySelectorAll('.map').forEach(( element) => {
+    const cw = element.getBoundingClientRect().width;
+    element.style.height = `${cw}px`;
+    element.style.maxHeight = '400px';
 
     const options = {
       mapTypeId: google.maps.MapTypeId.ROADMAP, // eslint-disable-line no-undef
@@ -126,18 +108,18 @@ const initMap = () => {
     }
 
     const map = new google.maps.Map(element, options); // eslint-disable-line no-undef
-    $(element).data('map', map)
-    $(element).trigger('update_map')
+    element.dataset.map = map;
+    element.dispatchEvent(new Event('update_map'))
   });
 
-  $('.collection_map').each((_, element) => {
-    const cw = $(element).width() - 100;
-    $(element).css({ 'height': cw + 'px' });
+  document.querySelectorAll('.collection_map').forEach((element) => {
+    const cw = element.getBoundingClientRect().width - 100;
+    element.style.height = `${cw}px`;
 
     const options = {
-      center: new google.maps.LatLng(5, 140), // eslint-disable-line no-undef
+      center: new google.maps.LatLng(5, 140),
       zoom: 1,
-      mapTypeId: google.maps.MapTypeId.ROADMAP, // eslint-disable-line no-undef
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true,
       scrollwheel: false,
       zoomControl: true,
@@ -145,47 +127,44 @@ const initMap = () => {
       disableDoubleClickZoom: false,
     };
 
-    const map = new google.maps.Map(element, options); // eslint-disable-line no-undef
-    $(element).data('map', map);
+    const map = new google.maps.Map(element, options);
+    element.dataset.map = map;
 
-    const north_limit = $(element).data('north-limit');
-    const south_limit = $(element).data('south-limit');
-    const east_limit = $(element).data('east-limit');
-    const west_limit = $(element).data('west-limit');
+    const north_limit = element.dataset.northLimit;
+    const south_limit = element.dataset.southLimit;
+    const east_limit = element.dataset.eastLimit;
+    const west_limit = element.dataset.westLimit;
     if (north_limit) {
-      const sw = new google.maps.LatLng(south_limit, west_limit); // eslint-disable-line no-undef
-      const ne = new google.maps.LatLng(north_limit, east_limit); // eslint-disable-line no-undef
-      const bounds = new google.maps.LatLngBounds(sw, ne); // eslint-disable-line no-undef
+      const sw = new google.maps.LatLng(south_limit, west_limit);
+      const ne = new google.maps.LatLng(north_limit, east_limit);
+      const bounds = new google.maps.LatLngBounds(sw, ne);
       map.fitBounds(bounds);
     }
 
-    const coordinates = $(element).data('coordinates');
-    // const url = $(element).data('url');
+    const coordinates = element.dataset.coordinates;
     const cluster_options = {
       gridSize: 15,
       maxZoom: 15,
       avgCenter: false,
       minClusterSize: 5
     }
-    const clusterer = new MarkerClusterer(map, [], cluster_options); // eslint-disable-line no-undef
+    const clusterer = new google.maps.MarkerClusterer(map, [], cluster_options);
     coordinates.forEach((coord) => {
-      const latlng = new google.maps.LatLng(coord['lat'], coord['lng']); // eslint-disable-line no-undef
+      const latlng = new google.maps.LatLng(coord.lat, coord.lng);
 
-      // const link = url + '/' + coord['id'];
-      // const items = coord['items'];
-      const content = $(element).data('content')
-        .replace(/TITLE/g, coord['title'])
-        .replace(/ID/g, coord['id'])
-        .replace(/ITEMS/g, coord['items']);
+      const content = element.dataset.content
+        .replace(/TITLE/g, coord.title)
+        .replace(/ID/g, coord.id)
+        .replace(/ITEMS/g, coord.items);
 
-      const marker = new google.maps.Marker({ // eslint-disable-line no-undef
+      const marker = new google.maps.Marker({
         position: latlng,
-        title: coord['title'],
+        title: coord.title,
         clickable: true
       });
-      marker.info = new google.maps.InfoWindow({ content: content }); // eslint-disable-line no-undef
+      marker.info = new google.maps.InfoWindow({ content: content });
 
-      google.maps.event.addListener(marker, 'click', (el) => { // eslint-disable-line no-undef
+      google.maps.event.addListener(marker, 'click', () => {
         marker.info.open(map, marker);
       });
 
