05af159a5bee7b42687668176bcffd139aa894c8
John Ferlito <johnf@inodes.org>
2023-03-19 18:27:00 +1100

    Move the google maps callback to it's own script so it is ready to be loaded #708

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,7 +10,6 @@ import 'vendor/jquery-query';
 import 'vendor/select2';
 import 'vendor/markerclusterer';
 
-import 'custom/maps';
 import 'custom/select2_setup';
 import 'custom/datepicker';
 import 'custom/sortable';
