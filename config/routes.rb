7114aefe5319b1e02cd812eb2bdd34787dbea07c
John Ferlito <johnf@inodes.org>
2023-03-20 18:56:34 +1100

    Fix logout #715

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 #
 
 Rails.application.routes.draw do
-  resources :foos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/paradisec.graphql', to: 'graphql#schema'
