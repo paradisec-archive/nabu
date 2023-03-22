08c4142e82058bd677292ff4c6c6ec4f85917be7
John Ferlito <johnf@inodes.org>
2023-03-22 22:12:36 +1100

    Debug maps on server

--- app/assets/javascript/maps.js
+++ app/assets/javascript/maps.js
@@ -1,11 +1,13 @@
 const initMap = () => {
   const set_map_bounds_from_ajax = (path, ids) => {
+    console.info('set_map_bounds_from_ajax', path, ids);
     const marker_bounds = new google.maps.LatLngBounds();
 
     ids.forEach(async (id) => {
       // TOOD: Do we have to set data type as json?
       const response = await fetch(`${path}${id}?location_only=true`);
       const data = await response.json();
+      console.info('data', data);
 
       if (!data || !data['north_limit']) {
         console.info('NO data', data);
@@ -18,6 +20,7 @@ const initMap = () => {
         east_limit,
         west_limit,
       } = data;
+      console.info('north_limit', north_limit);
 
       const sw = new google.maps.LatLng(south_limit, west_limit);
       const ne = new google.maps.LatLng(north_limit, east_limit);
@@ -27,6 +30,9 @@ const initMap = () => {
 
     const ne = marker_bounds.getNorthEast();
     const sw = marker_bounds.getSouthWest();
+
+    console.info('marker_bounds', marker_bounds);
+    console.info('marker_bounds', marker_bounds.isEmpty());
 
     if (marker_bounds.isEmpty()) {
       const node = document.querySelector('.no-map-match-message');
