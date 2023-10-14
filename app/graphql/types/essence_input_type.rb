203589ac6f516772c214f5930c4773979905c46f
John Ferlito <johnf@inodes.org>
2023-10-14 15:07:52 +1100

    graphql: more fields on createEssence

--- app/graphql/types/essence_input_type.rb
+++ app/graphql/types/essence_input_type.rb
@@ -6,17 +6,12 @@ module Types
     argument :item_identifier, String
     argument :collection_identifier, String
     argument :filename, String
-    # field :mimetype, String
-    # field :bitrate, Integer
-    # field :samplerate, Integer
-    # field :size, Integer
-    # field :duration, Float
-    # field :channels, Integer
-    # field :fps, Integer
-    # field :doi, String
-    # field :derived_files_generated, Boolean
-    # field :doi_xml, String, method: :to_doi_xml, camelize: false
-    # field :citation, String
-    # field :permalink, String, method: :full_path
+    argument :mimetype, String
+    argument :bitrate, Integer
+    argument :samplerate, Integer
+    argument :size, Integer
+    argument :duration, Float
+    argument :channels, Integer
+    argument :fps, Integer
   end
 end
