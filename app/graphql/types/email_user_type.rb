265e3026bd5421f0434aad63762acd5137d159a4
John Ferlito <johnf@inodes.org>
2023-11-05 17:43:52 +1100

    Add naames to user graphql query

--- app/graphql/types/email_user_type.rb
+++ app/graphql/types/email_user_type.rb
@@ -3,6 +3,8 @@
 module Types
   class EmailUserType < Types::BaseObject
     field :email, String
+    field :first_name, String, null: true, camelize: false
+    field :last_name, String, null: true, camelize: false
 
     def self.authorized?(object, context)
       super && context[:viewer].admin?
