2ab59f178a475b608c4961c6380f167fb6a2b775
John Ferlito <johnf@inodes.org>
2024-02-18 16:48:38 +1100

    format + rubocop

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,56 +1,56 @@
 class PageController < ApplicationController
   def home
-    @page_title = "Nabu - Home"
+    @page_title = 'Nabu - Home'
     item_counts = Item.group(:collection_id).count
-    @coordinates = Collection.where(:private => false).map{|c| c.center_coordinate(item_counts)}.compact
-    @content = render_to_string :partial => 'page/infowindow'
+    @coordinates = Collection.where(private: false).map { |c| c.center_coordinate(item_counts) }.compact
+    @content = render_to_string partial: 'page/infowindow'
   end
 
   def dashboard
     authenticate_user!
 
-    @page_title = "Nabu - Dashboard"
+    @page_title = 'Nabu - Dashboard'
     @name = current_user.name
 
-    collections = Collection.where('collector_id = :user_id OR operator_id = :user_id', :user_id => current_user.id)
-    if (params[:sort])
-      collections = collections.order(params[:sort] + ' ' + params[:direction])
-    else
-      collections = collections.order('created_at desc')
-    end
+    collections = Collection.where('collector_id = :user_id OR operator_id = :user_id', user_id: current_user.id)
+    collections = if params[:sort]
+                    collections.order("#{params[:sort]} #{params[:direction]}")
+                  else
+                    collections.order('created_at desc')
+                  end
     @num_collections = collections.count
     @collections = collections.page(params[:collections_page]).per(params[:collections_per_page])
 
-    items = Item.where(:collector_id => current_user).order(:updated_at)
+    items = Item.where(collector_id: current_user).order(:updated_at)
     @num_items = items.count
     @items = items.page(params[:items_page]).per(params[:items_per_page])
 
     @comments = Comment.owned_by(current_user)
     @num_comments = @comments.count
 
-    @comments_left = Item.where(:collector_id => current_user).map(&:comments).flatten
+    @comments_left = Item.where(collector_id: current_user).map(&:comments).flatten
     @num_comments_left = @comments_left.count
 
     item_counts = Item.group(:collection_id).count
-    @coordinates = @collections.map{|c| c.center_coordinate(item_counts)}.compact
-    @north_limit = @coordinates.map{|c| c[:lat]}.max
-    @south_limit = @coordinates.map{|c| c[:lat]}.min
-    @east_limit  = @coordinates.map{|c| c[:lng]}.max
-    @west_limit  = @coordinates.map{|c| c[:lng]}.min
-    @content = render_to_string :partial => 'page/infowindow'
+    @coordinates = @collections.map { |c| c.center_coordinate(item_counts) }.compact
+    @north_limit = @coordinates.pluck(:lat).max
+    @south_limit = @coordinates.pluck(:lat).min
+    @east_limit  = @coordinates.pluck(:lng).max
+    @west_limit  = @coordinates.pluck(:lng).min
+    @content = render_to_string partial: 'page/infowindow'
   end
 
   def glossary
-    @page_title = "Nabu - Glossary"
+    @page_title = 'Nabu - Glossary'
     @universities = University.all
-    @fundingBodies = FundingBody.all
+    @funding_bodies = FundingBody.all
   end
 
   def apidoc
-    @page_title = "Nabu - API Documentation"
+    @page_title = 'Nabu - API Documentation'
   end
 
   def contact
-    @page_title = "Nabu - Contact"
+    @page_title = 'Nabu - Contact'
   end
 end
