3d7625dd8c90a6abc581ca6fb0443514aea2e143
John Ferlito <johnf@inodes.org>
2024-02-18 16:02:18 +1100

    format

--- app/admin/catalog_report.rb
+++ app/admin/catalog_report.rb
@@ -1,6 +1,4 @@
 ActiveAdmin.register_page 'Catalog Report' do
-
-
   content do
     year = params[:year] || Date.today.year
     month = params[:month] || Date.today.month
@@ -9,19 +7,19 @@ ActiveAdmin.register_page 'Catalog Report' do
     date = Date.parse("#{year}-#{month}-01")
 
     data = {
-      :new_collections => Collection.where('created_at >= ? AND created_at <= ?', date.beginning_of_month, date.end_of_month),
-      :new_items       => Item.where('created_at >= ? AND created_at <= ?', date.beginning_of_month, date.end_of_month),
-      :new_essences    => Essence.where('created_at >= ? AND created_at <= ?', date.beginning_of_month, date.end_of_month),
+      new_collections: Collection.where('created_at >= ? AND created_at <= ?', date.beginning_of_month, date.end_of_month),
+      new_items: Item.where('created_at >= ? AND created_at <= ?', date.beginning_of_month, date.end_of_month),
+      new_essences: Essence.where('created_at >= ? AND created_at <= ?', date.beginning_of_month, date.end_of_month),
 
-      :updated_collections => Collection.where('updated_at >= ? AND updated_at <= ?', date.beginning_of_month, date.end_of_month),
-      :updated_items       => Item.where('updated_at >= ? AND updated_at <= ?', date.beginning_of_month, date.end_of_month),
-      :updated_essences    => Essence.where('updated_at >= ? AND updated_at <= ?', date.beginning_of_month, date.end_of_month),
+      updated_collections: Collection.where('updated_at >= ? AND updated_at <= ?', date.beginning_of_month, date.end_of_month),
+      updated_items: Item.where('updated_at >= ? AND updated_at <= ?', date.beginning_of_month, date.end_of_month),
+      updated_essences: Essence.where('updated_at >= ? AND updated_at <= ?', date.beginning_of_month, date.end_of_month)
     }
 
     columns do
       column do
         panel "Catalog report for #{date.strftime('%B %Y')}" do
-          render :partial => 'form', :locals => {:date => date, :year  => year, :month => month}
+          render partial: 'form', locals: { date:, year:, month: }
         end
       end
     end
@@ -29,21 +27,21 @@ ActiveAdmin.register_page 'Catalog Report' do
     columns do
       column do
         panel 'Summary' do
-          render :partial => 'summary', :locals => {:date => date, :data => data}
+          render partial: 'summary', locals: { date:, data: }
         end
       end
 
       column do
         panel 'Statistics' do
           div do
-            render :partial => 'admin/dashboard/statistics', :locals => {:date => date}
+            render partial: 'admin/dashboard/statistics', locals: { date: }
           end
         end
       end
     end
 
     columns do
-      ['new', 'updated'].each do |which|
+      %w[new updated].each do |which|
         column do
           panel "#{which.capitalize} Collections" do
             insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, data[:"#{which}_collections"] do
@@ -58,7 +56,7 @@ ActiveAdmin.register_page 'Catalog Report' do
     end
 
     columns do
-      ['new', 'updated'].each do |which|
+      %w[new updated].each do |which|
         column do
           panel "#{which.capitalize} Items" do
             insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, data[:"#{which}_items"] do
@@ -73,7 +71,7 @@ ActiveAdmin.register_page 'Catalog Report' do
     end
 
     columns do
-      ['new', 'updated'].each do |which|
+      %w[new updated].each do |which|
         column do
           panel "#{which.capitalize} Files" do
             insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, data[:"#{which}_essences"] do
@@ -89,18 +87,18 @@ ActiveAdmin.register_page 'Catalog Report' do
     columns do
       column do
         panel 'File Type Metrics' do
-          stuff = Essence.where('created_at <= ?', date.end_of_month).select('mimetype, COUNT(*) as files, SUM(size) as bytes, SUM(duration) as duration').group(:mimetype).order('files desc')
+          stuff = Essence.where('created_at <= ?',
+                                date.end_of_month).select('mimetype, COUNT(*) as files, SUM(size) as bytes, SUM(duration) as duration').group(:mimetype).order('files desc')
           insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, stuff do
             column :type
             column :files
-            column :bytes, :as => 'Size' do |thing|
+            column :bytes, as: 'Size' do |thing|
               number_to_human_size thing.bytes
             end
             column :duration do |thing|
               number_to_human_duration thing.duration
             end
           end
-
         end
       end
 
@@ -113,18 +111,17 @@ ActiveAdmin.register_page 'Catalog Report' do
             end
 
             column :files do |collection|
-              Essence.where('created_at <= ?', date.end_of_month).where(:item_id => collection.items.map(&:id)).count
+              Essence.where('created_at <= ?', date.end_of_month).where(item_id: collection.items.map(&:id)).count
             end
-            column :bytes, :as => 'Size' do |collection|
-              number_to_human_size Essence.where('created_at <= ?', date.end_of_month).where(:item_id => collection.items.map(&:id)).sum(:size)
+            column :bytes, as: 'Size' do |collection|
+              number_to_human_size Essence.where('created_at <= ?', date.end_of_month).where(item_id: collection.items.map(&:id)).sum(:size)
             end
             column :duration do |collection|
-              number_to_human_duration Essence.where('created_at <= ?', date.end_of_month).where(:item_id => collection.items.map(&:id)).sum(:duration)
+              number_to_human_duration Essence.where('created_at <= ?', date.end_of_month).where(item_id: collection.items.map(&:id)).sum(:duration)
             end
           end
         end
       end
     end
   end
-
 end
