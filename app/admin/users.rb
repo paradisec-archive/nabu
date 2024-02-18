d36337ed242823ee97bd7d790eaf683acf1b0786
John Ferlito <johnf@inodes.org>
2024-02-18 18:11:17 +1100

    rubocop + format

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -53,6 +53,7 @@
 # * `index_users_on_unlock_token` (_unique_):
 #     * **`unlock_token`**
 #
+# rubocop:disable Metrics/BlockLength
 ActiveAdmin.register User do
   # show scoped buttons on index page
   scope :users
@@ -61,26 +62,24 @@ ActiveAdmin.register User do
   scope :collectors
 
   permit_params :party_identifiers_attributes, :email, :first_name, :last_name,
-    :address, :address2, :country, :phone, :password, :password_confirmation, :remember_me, :unconfirmed_email,
-    :rights_transferred_to_id, :rights_transfer_reason, :admin, :contact_only, :party_identifier, :collector, :unikey
+                :address, :address2, :country, :phone, :password, :password_confirmation, :remember_me, :unconfirmed_email,
+                :rights_transferred_to_id, :rights_transfer_reason, :admin, :contact_only, :party_identifier, :collector, :unikey
 
   before_destroy :check_dependent
 
   controller do
     def check_dependent(object)
-      if object.owned_items.count > 0
-        flash[:error] = "ERROR: User owns items - cannot be removed."
-        return false
-      end
+      return unless object.owned_items.positive?
+
+      flash[:error] = 'ERROR: User owns items - cannot be removed.'
+      false
     end
   end
 
   # add pagination buttons to index page sidebar
-  sidebar :paginate, :only => :index  do
+  sidebar :paginate, only: :index do
     count = User.count
-    unless params[:scope].blank?
-      count = User.public_send(params[:scope].to_sym).count
-    end
+    count = User.public_send(params[:scope].to_sym).count if params[:scope].present?
 
     ['10', '50', "all #{count}"].each do |n|
       para link_to "Show #{n}", params.permit!.merge(per_page: n.sub('all ', ''), page: n.start_with?('all') ? 1 : params[:page]), class: 'button'
@@ -88,7 +87,7 @@ ActiveAdmin.register User do
   end
 
   # change pagination
-  before_action :only => :index do
+  before_action only: :index do
     @per_page = params[:per_page] || 30
   end
 
@@ -108,17 +107,13 @@ ActiveAdmin.register User do
   filter :admin
 
   action_item :merge do
-    if %w(show edit).include?(params[:action])
-      if User.duplicates_of(resource.first_name, resource.last_name).count > 1
-        link_to 'Merge User', merge_admin_user_path, style: 'float: right;'
-      end
+    if %w[show edit].include?(params[:action]) && (User.duplicates_of(resource.first_name, resource.last_name).count > 1)
+      link_to 'Merge User', merge_admin_user_path, style: 'float: right;'
     end
   end
 
   action_item :reset do
-    if %w(show edit).include?(params[:action])
-      link_to 'Reset Password', reset_password_admin_user_path, style: 'float: right;'
-    end
+    link_to 'Reset Password', reset_password_admin_user_path, style: 'float: right;' if %w[show edit].include?(params[:action])
   end
 
   member_action :reset_password, method: :get do
@@ -140,7 +135,7 @@ ActiveAdmin.register User do
     @duplicates = User.duplicates_of(@primary_user.first_name, @primary_user.last_name, params[:specific_user_ids].try(:split, ','))
     @merge_user = MergeUser.new(@duplicates)
     # so we don't merge the primary one
-    @duplicates = @duplicates.reject {|d| d.id == @primary_user.id}
+    @duplicates = @duplicates.reject { |d| d.id == @primary_user.id }
   end
 
   member_action :do_merge, method: :patch do
@@ -151,8 +146,9 @@ ActiveAdmin.register User do
       # get the updated parameters from the form merge
       @primary_user.assign_attributes(params[:user], as: :admin)
 
-      if ids_to_merge and ids_to_merge.present? and UserMergerService.new(@primary_user, User.find(ids_to_merge)).call
-        redirect_to edit_admin_user_path, notice: "Successfully merged user! [#{@primary_user.name}]" and return
+      if ids_to_merge.present? && UserMergerService.new(@primary_user, User.find(ids_to_merge)).call
+        redirect_to edit_admin_user_path, notice: "Successfully merged user! [#{@primary_user.name}]"
+        return
       else
         flash[:alert] = 'Must select 1 or more duplicate users to perform merge'
       end
@@ -174,7 +170,7 @@ ActiveAdmin.register User do
     column :contact_only
     column :admin
     column :party_identifier
-    #TODO: uncomment once this stuff is tested in staging
+    # TODO: uncomment once this stuff is tested in staging
     # column :party_identifiers do |user|
     #   user.party_identifiers.map{|p| p.identifier}.join(', ')
     # end
@@ -184,7 +180,7 @@ ActiveAdmin.register User do
   end
 
   # show page
-  show do |user|
+  show do |_user|
     attributes_table do
       row :id
       row :first_name
@@ -202,13 +198,13 @@ ActiveAdmin.register User do
       row :admin
       row :party_identifier
       row :unikey
-      #TODO: uncomment once this stuff is tested in staging
+      # TODO: uncomment once this stuff is tested in staging
       # row :party_identifiers do |user|
       #   user.party_identifiers.map{|p| p.identifier}.join("\n")
       # end
     end
 
-    h3 "Admin information"
+    h3 'Admin information'
     attributes_table do
       row :created_at
       row :confirmation_sent_at
@@ -227,7 +223,7 @@ ActiveAdmin.register User do
 
   # edit page
   form do |f|
-    f.inputs "User Details" do
+    f.inputs 'User Details' do
       f.input :first_name
       f.input :last_name
       f.input :address
@@ -247,7 +243,7 @@ ActiveAdmin.register User do
       end
       f.input :party_identifier
       f.input :unikey
-      #TODO: uncomment once this stuff is tested in staging
+      # TODO: uncomment once this stuff is tested in staging
       # f.has_many :party_identifiers, allow_destroy: true do |p|
       #   p.input :user_id, as: :hidden, input_html: {value: f.object.id}
       #   p.input :party_type, as: :select, collection: PartyIdentifier::TYPES.each_with_index.map{|t,i| [t,i]}
@@ -273,9 +269,10 @@ ActiveAdmin.register User do
     column :contact_only
     column :admin
     column :party_identifier
-    #TODO: uncomment once this stuff is tested in staging
+    # TODO: uncomment once this stuff is tested in staging
     # column :party_identifiers do |user|
     #   user.party_identifiers.map{|p| p.identifier}.join(';')
     # end
   end
 end
+# rubocop:enable Metrics/BlockLength
