8631aaebb51bd40086ddd2d3637df4c723ed0c6a
John Ferlito <johnf@inodes.org>
2024-02-21 20:12:43 +1100

    Fix error in file processing

--- app/admin/file_processing.rb
+++ app/admin/file_processing.rb
@@ -1,28 +1,28 @@
-ActiveAdmin.register_page "File Processing" do
+# rubocop:disable Metrics/BlockLength
+ActiveAdmin.register_page 'File Processing' do
   content do
-    div :class => 'dashboard_section panel' do
+    div class: 'dashboard_section panel' do
       h3 'Collections with at least one item not ready for export (max 20)'
-      div :class => 'panel_contents' do
-        insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, Collection.includes(:items).where(:items => {:metadata_exportable => false}).order('collections.id desc').limit(20) do
-          column :identifier
-          # FIXME USe _path for the URLs below
-          column :collection_id do |collection|
-            link_to collection.identifier, Rails.application.routes.url_helpers.collection_path(collection) # Have to call the full path here as activeadmin has a collection_path
+      div class: 'panel_contents' do
+        table_for Collection.includes(:items).where(items: { metadata_exportable: false }).order('collections.identifier asc').limit(20) do
+          # FIXME: Use _path for the URLs below
+          column :collection do |collection|
+            # Have to call the full path here as activeadmin has a collection_path
+            link_to collection.identifier, Rails.application.routes.url_helpers.collection_path(collection)
           end
           column :sample_item do |collection|
-            item = collection.items.where(:metadata_exportable => false).first
+            item = collection.items.where(metadata_exportable: false).first
             link_to item.identifier, [collection, item]
           end
           column :title
-          actions
         end
       end
     end
 
-    div :class => 'dashboard_section panel' do
+    div class: 'dashboard_section panel' do
       h3 'Recently imported files (max 20)'
-      div :class => 'panel_contents' do
-        insert_tag ActiveAdmin::Views::IndexAsTable::IndexTableFor, Essence.order('id desc').limit(20) do
+      div class: 'panel_contents' do
+        table_for Essence.order('id desc').limit(20) do
           column :id do |essence|
             link_to essence.id, [essence.item.collection, essence.item, essence]
           end
@@ -34,14 +34,14 @@ ActiveAdmin.register_page "File Processing" do
           column :channels
           column :fps
           column :bitrate
-          actions
         end
       end
     end
 
-    div :class => 'dashboard_section panel' do
+    div class: 'dashboard_section panel' do
       render 'paths'
       para 'Note: the machine is set up to check directories once every 5 minutes.'
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
