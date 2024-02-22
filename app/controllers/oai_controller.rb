df5a4a2040762d3fa8b610077ba47b7226e446f6
John Ferlito <johnf@inodes.org>
2024-02-22 17:50:28 +1100

    rubocop

--- app/controllers/oai_controller.rb
+++ app/controllers/oai_controller.rb
@@ -5,26 +5,27 @@ class OaiController < ApplicationController
     provider = ItemProvider.new
 
     options = create_options('item')
-    response =  provider.process_request(options)
+    response = provider.process_request(options)
 
-    render :body => response, :content_type => 'text/xml'
+    render body: response, content_type: 'text/xml'
   end
 
   def collection
     provider = CollectionProvider.new
 
     options = create_options('collection')
-    response =  provider.process_request(options)
+    response = provider.process_request(options)
 
-    render :body => response, :content_type => 'text/xml'
+    render body: response, content_type: 'text/xml'
   end
 
   private
+
   def permitted_params
     params.permit(:verb, :identifier, :metadataPrefix, :set, :from, :until, :resumptionToken)
   end
 
-  def create_options(model)
+  def create_options(_model)
     params = permitted_params.to_h
     params.merge!(url: "#{request.base_url}#{request.path}")
 
