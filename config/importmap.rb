d9cd46e742789fc130371b0a8a65b100d6f0a239
John Ferlito <johnf@inodes.org>
2024-04-25 13:40:20 +1000

    FIx maps

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin 'application', preload: true
+pin 'maps'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
