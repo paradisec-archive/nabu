d1d178f0635ed1e86ec308753cdffacfc6655cfb
John Ferlito <johnf@inodes.org>
2024-09-05 09:43:25 +1000

    FIx user merging

--- app/services/user_merger_service.rb
+++ app/services/user_merger_service.rb
@@ -2,7 +2,7 @@ class UserMergerService
   def initialize(user, duplicates)
     # make sure we don't accidentally destroy target user
     @user = user
-    @duplicates = duplicates.present? ? duplicates.reject {|d| d.id == user.id} : nil
+    @duplicates = duplicates.present? ? duplicates.reject { |d| d.id == user.id } : nil
   end
 
   def call
@@ -22,11 +22,11 @@ class UserMergerService
   def reassign_ownership(dup_ids)
     Rails.logger.debug "Reassigning item permissions from #{dup_ids.inspect} to #{@user.id}"
 
-    #set all fields referencing old duplicates to point to new primary user
-    Item.update_all({collector_id: @user.id}, {collector_id: dup_ids})
-    Item.update_all({operator_id: @user.id}, {operator_id: dup_ids})
-    ItemUser.update_all({user_id: @user.id}, {user_id: dup_ids})
-    ItemAdmin.update_all({user_id: @user.id}, {user_id: dup_ids})
-    ItemAgent.update_all({user_id: @user.id}, {user_id: dup_ids})
+    # set all fields referencing old duplicates to point to new primary user
+    Item.where(collector_id: dup_ids).update_all(collector_id: @user.id)
+    Item.where(operator_id: dup_ids).update_all(operator_id: @user.id)
+    ItemUser.where(user_id: dup_ids).update_all(user_id: @user.id)
+    ItemAdmin.where(user_id: dup_ids).update_all(user_id: @user.id)
+    ItemAgent.where(user_id: dup_ids).update_all(user_id: @user.id)
   end
 end
