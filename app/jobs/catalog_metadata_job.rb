61b33284a002533d591544533b63a28c5fcb60dd
John Ferlito <johnf@inodes.org>
2024-10-28 18:21:25 +1100

    Fix metadata render

--- app/jobs/catalog_metadata_job.rb
+++ app/jobs/catalog_metadata_job.rb
@@ -4,15 +4,14 @@ class CatalogMetadataJob < ApplicationJob
   def perform(data, is_item)
     local_data = { data:, is_item:, admin_rocrate: true }
 
-    rocrate = Api::V1::OniController.render :object_meta, assigns: local_data
 
     filename = 'ro-crate-metadata.json'
 
     if is_item
-      # data = data.includes(:content_languages, :subject_languages, item_agents: %i[agent_role user])
+      rocrate = Api::V1::OniController.render :object_meta_item, assigns: local_data
       Nabu::Catalog.instance.upload_item_admin(data, filename, rocrate, 'application/json')
     else
-      # data = data.includes(items: [:admins, :users]) # => { :content_languages, :subject_languages, item_agents: %i[agent_role user] } })
+      rocrate = Api::V1::OniController.render :object_meta_collection, assigns: local_data
       Nabu::Catalog.instance.upload_collection_admin(data, filename, rocrate, 'application/json')
     end
   end
