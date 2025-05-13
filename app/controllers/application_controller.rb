3c10f7331131f22111041aee0dad367f0b74775d
John Ferlito <johnf@inodes.org>
2025-05-13 13:48:51 +1000

    chore: reorder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,63 @@ class ApplicationController < ActionController::Base
 
   private
 
+  ##########
+  # For Active Admin
+  ##########
+
+  def authenticate_admin_user!
+    redirect_to new_user_session_path unless current_user&.admin?
+  end
+
+  def current_admin_user
+    current_user
+  end
+
+  ##########
+  # Devise redirect after login
+  ##########
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || dashboard_path
+  end
+
+  ##########
+  # CanCan
+  ##########
+
+  rescue_from CanCan::AccessDenied do |exception|
+    # If it's a JSON request, give a 40x rather than redirecting them
+    if request.format.symbol == :json && current_user
+      render nothing: true, status: :forbidden
+    elsif request.format.symbol == :json
+      render nothing: true, status: :unauthorized
+    elsif current_user
+      redirect_to root_url, alert: exception.message
+    else
+      session['user_return_to'] = request.fullpath
+      redirect_to new_user_session_path, alert: exception.message
+    end
+  end
+
+  ##########
+  # Before Actions
+  ##########
+
+  def set_timezone
+    Time.zone = current_user.time_zone if current_user
+  end
+
+  def set_access_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
+  def set_sentry_user
+    Sentry.set_user(id: current_user.id, email: current_user.email) if current_user
+  end
+
   def validate_per_page_param
     fields = %i[per_page items_page page files_per_page]
     fields.each do |name|
@@ -28,41 +85,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  rescue_from CanCan::AccessDenied do |exception|
-    # If it's a JSON request, give a 40x rather than redirecting them
-    if request.format.symbol == :json && current_user
-      render nothing: true, status: :forbidden
-    elsif request.format.symbol == :json
-      render nothing: true, status: :unauthorized
-    elsif current_user
-      redirect_to root_url, alert: exception.message
-    else
-      session['user_return_to'] = request.fullpath
-      redirect_to new_user_session_path, alert: exception.message
-    end
-  end
-
-  def set_timezone
-    Time.zone = current_user.time_zone if current_user
-  end
-
-  def set_sentry_user
-    Sentry.set_user(id: current_user.id, email: current_user.email) if current_user
-  end
-
-  def authenticate_admin_user!
-    redirect_to new_user_session_path unless current_user&.admin?
-  end
-
-  def current_admin_user
-    current_user
-  end
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || dashboard_path
-  end
-
+  ############
   # used by collections_controller and items_controller for creating Collectors and Agents
+  ############
   def create_contact(name)
     name = name.gsub(/^NEWCONTACT:/, '')
 
@@ -85,13 +110,7 @@ class ApplicationController < ActionController::Base
                                contact_only: true
                              })
     end
-    contact.id
-  end
 
-  def set_access_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    contact.id
   end
 end
