bc7cc4fa023be9ffc210d4364bd39acbf9bda6c6
John Ferlito <johnf@inodes.org>
2025-08-08 09:30:30 +1000

    Add user agent to log tags

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
   # Log to STDOUT with the current request id as a default log tag.
-  config.log_tags = [:request_id]
+  config.log_tags = [:request_id, :remote_ip, :user_agent]
   config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
