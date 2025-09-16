075629ee1558140f4020248a26afe819cf0a1c77
John Ferlito <johnf@inodes.org>
2025-09-16 16:39:20 +1000

    fix: bad unikjey fields

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -18,16 +18,73 @@ module Types
       argument :identifier, ID
     end
 
+    field :item, ItemType, 'Find an item by full identifier. e.g. NT1-009' do
+      argument :full_identifier, ID
+    end
+
+
+    field :item_bwf_csv, ItemBwfCsvType, 'Get the BWF CSV for an item' do
+      argument :filename, String
+      argument :full_identifier, ID
+    end
+
+
+    field :item_id3, ItemId3Type, 'Get the ID3 XML for an item' do
+      argument :full_identifier, ID
+    end
+
+
+    field :items, Types::ItemResultType, null: true do
+      argument :limit, Integer, default_value: 10, required: false
+      argument :page, Integer, default_value: 1, required: false
+      # argument :sort_order, String, default_value: :full_identifier, required: false, camelize: false
+      argument :access_class, String, required: false, camelize: false
+      argument :access_condition_name, String, required: false, camelize: false
+      argument :access_narrative, String, required: false, camelize: false
+      argument :born_digital, Boolean, required: false, camelize: false
+      argument :collection_identifier, String, required: false, camelize: false
+      argument :collector_name, String, required: false, camelize: false
+      argument :description, String, required: false
+      argument :dialect, String, required: false
+      argument :digitised_on, String, required: false, camelize: false
+      argument :discourse_type_name, String, required: false, camelize: false
+      argument :doi, String, required: false
+      argument :essences_count, Integer, required: false, camelize: false
+      argument :full_identifier, String, required: false, camelize: false
+      argument :id, ID, required: false
+      argument :identifier, String, required: false
+      argument :ingest_notes, String, required: false, camelize: false
+      argument :language, String, required: false
+      argument :operator_name, String, required: false, camelize: false
+      argument :original_media, String, required: false, camelize: false
+      argument :originated_on, String, required: false, camelize: false
+      argument :originated_on_narrative, String, required: false, camelize: false
+      argument :private, Boolean, required: false
+      argument :received_on, String, required: false, camelize: false
+      argument :region, String, required: false
+      argument :title, String, required: false
+      argument :tracking, String, required: false
+      argument :university_name, String, required: false, camelize: false
+    end
+
+
+    field :essence, EssenceType, 'Find a collection by identifier. e.g. NT1' do
+      argument :filename, String
+      argument :full_identifier, ID
+    end
+
+
+    field :user_by_unikey, EmailUserType, 'Find a user by their unikey' do
+      argument :unikey, String
+    end
+
+
     def collection(identifier:)
       c = Collection.find_by!(identifier:)
 
       authorize! :read, c
 
       c
-    end
-
-    field :item, ItemType, 'Find an item by full identifier. e.g. NT1-009' do
-      argument :full_identifier, ID
     end
 
     def item(full_identifier:)
@@ -37,11 +94,6 @@ module Types
       authorize! :read, c
 
       c.items.accessible_by(current_ability).find_by!(identifier: item_identifier)
-    end
-
-    field :item_bwf_csv, ItemBwfCsvType, 'Get the BWF CSV for an item' do
-      argument :full_identifier, ID
-      argument :filename, String
     end
     def item_bwf_csv(full_identifier:, filename:)
       collection_identifier, item_identifier = full_identifier.split('-')
@@ -87,10 +139,6 @@ module Types
         updated_at: i.updated_at
       }
     end
-
-    field :item_id3, ItemId3Type, 'Get the ID3 XML for an item' do
-      argument :full_identifier, ID
-    end
     def item_id3(full_identifier:)
       collection_identifier, item_identifier = full_identifier.split('-')
 
@@ -121,39 +169,6 @@ module Types
       }
     end
 
-    field :items, Types::ItemResultType, null: true do
-      argument :limit, Integer, default_value: 10, required: false
-      argument :page, Integer, default_value: 1, required: false
-      # argument :sort_order, String, default_value: :full_identifier, required: false, camelize: false
-      argument :title, String, required: false
-      argument :identifier, String, required: false
-      argument :full_identifier, String, required: false, camelize: false
-      argument :collection_identifier, String, required: false, camelize: false
-      argument :collector_name, String, required: false, camelize: false
-      argument :university_name, String, required: false, camelize: false
-      argument :operator_name, String, required: false, camelize: false
-      argument :discourse_type_name, String, required: false, camelize: false
-      argument :description, String, required: false
-      argument :language, String, required: false
-      argument :dialect, String, required: false
-      argument :region, String, required: false
-      argument :access_narrative, String, required: false, camelize: false
-      argument :tracking, String, required: false
-      argument :ingest_notes, String, required: false, camelize: false
-      argument :born_digital, Boolean, required: false, camelize: false
-      argument :originated_on, String, required: false, camelize: false
-      argument :essences_count, Integer, required: false, camelize: false
-      argument :id, ID, required: false
-      argument :access_class, String, required: false, camelize: false
-      argument :access_condition_name, String, required: false, camelize: false
-      argument :original_media, String, required: false, camelize: false
-      argument :received_on, String, required: false, camelize: false
-      argument :digitised_on, String, required: false, camelize: false
-      argument :originated_on_narrative, String, required: false, camelize: false
-      argument :doi, String, required: false
-      argument :private, Boolean, required: false
-    end
-
     def items(**args)
       @params = {
         # 500 seems to be the hard limit without server timing out
@@ -170,11 +185,6 @@ module Types
       )
     end
 
-    field :essence, EssenceType, 'Find a collection by identifier. e.g. NT1' do
-      argument :full_identifier, ID
-      argument :filename, String
-    end
-
     def essence(full_identifier:, filename:)
       collection_identifier, item_identifier = full_identifier.split('-')
 
@@ -187,15 +197,11 @@ module Types
       i.essences.accessible_by(current_ability).find_by!(filename:)
     end
 
-    field :user_by_unikey, EmailUserType, 'Find a user by their unikey' do
-      argument :unikey, String
-    end
-
     def user_by_unikey(unikey:)
       u = User.find_by!(unikey:)
       authorize! :read, u
 
-      { id: u.id, unikey: u.unikey, firstName: u.firstName, lastName: u.lastName, email: u.email }
+      { id: u.id, unikey: u.unikey, firstName: u.first_name, lastName: u.last_name, email: u.email }
     end
   end
 end
