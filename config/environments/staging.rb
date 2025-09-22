78af5c0ac354919bfeedbfe0cc59dff3809a49c8
John Ferlito <johnf@inodes.org>
2025-09-22 11:51:47 +1000

    feat: enable debug logging in stage

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,4 +2,10 @@ require_relative 'production'
 
 Nabu::Application.configure do
   config.action_mailer.default_url_options = { host: 'nabu-stage.paradisec.org.au' }
+
+  # Enable more logging in staging
+  config.active_record.verbose_query_logs = true
+  config.active_record.query_log_tags_enabled = true
+  config.active_job.verbose_enqueue_logs = true
+  config.log_level = :debug
 end
