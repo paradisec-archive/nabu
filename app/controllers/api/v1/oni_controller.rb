51f9dc20747f45bd5552579df7ee84aa07c3d613
John Ferlito <johnf@inodes.org>
2025-09-29 12:31:19 +1000

    use the api controller for oni

--- app/controllers/api/v1/oni_controller.rb
+++ app/controllers/api/v1/oni_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class OniController < ApplicationController
+    class OniController < ApiController
       def entities
         query = Oni::ObjectsValidator.new(params)
 
