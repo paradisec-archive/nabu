6b8e7e0a6c1a90aaf5fda62068acf631a7dbf47e
John Ferlito <johnf@inodes.org>
2026-02-12 14:57:57 +1100

    refactor(devise): modernise Devise config for Hotwire/Turbo compatibility
    Remove unnecessary Devise view overrides (sessions, passwords,
    confirmations, unlocks, shared partials) to use Devise defaults. Update
    devise.rb to use Turbo-compatible error/redirect statuses and remove
    legacy Turbolinks config. Fix reCAPTCHA error handling in registrations
    controller and pass reCAPTCHA env vars through docker-compose.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,28 +1,29 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters
   prepend_before_action :check_bitcoin, only: [:create]
   prepend_before_action :check_captcha, only: [:create]
+  prepend_before_action :configure_permitted_parameters
 
   private
 
   def check_captcha
     return if verify_recaptcha
 
+    flash.delete(:recaptcha_error)
+
     self.resource = resource_class.new sign_up_params
     resource.validate
     set_minimum_password_length
 
     respond_with_navigational(resource) do
-      flash.discard(:recaptcha_error)
+      resource.errors.add(:base, 'reCAPTCHA verification failed. Please try again.')
       render :new
     end
   end
 
   def check_bitcoin
     if params[:user][:first_name].match(/bitcoin/i) || params[:user][:last_name].match(/bitcoin/i)
-      flash[:error] = "You have used a banned first name, contact support if you think this is an error"
+      flash[:error] = 'You have used a banned first name, contact support if you think this is an error'
       redirect_to root_path
-      return
     end
   end
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -157,6 +157,9 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
+  # Also, when used in conjunction with `send_email_changed_notification`,
+  # the notification is sent to the original email when the change is requested,
+  # not when the unconfirmed email is confirmed.
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
@@ -256,16 +259,14 @@ Devise.setup do |config|
 
   # ==> Navigation configuration
   # Lists the formats that should be treated as navigational. Formats like
-  # :html, should redirect to the sign in page when the user does not have
+  # :html should redirect to the sign in page when the user does not have
   # access, but formats like :xml or :json, should return 401.
   #
   # If you have any extra navigational formats, like :iphone or :mobile, you
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # TODO: https://github.com/heartcombo/devise/issues/5439 and a bunch of releated bugs
-  # We uncommented this and added :turbo_stream to the end
-  config.navigational_formats = ['*/*', :html, :turbo_stream]
+  # config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -279,9 +280,9 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
+  # config.warden do |warden_config|
+  #   warden_config.intercept_401 = false
+  #   warden_config.default_strategies(scope: :user).unshift :some_external_strategy
   # end
 
   # ==> Mountable engine configurations
@@ -298,12 +299,14 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  # ==> Turbolinks configuration
-  # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
-  #
-  # ActiveSupport.on_load(:devise_failure_app) do
-  #   include Turbolinks::Controller
-  # end
+  # ==> Hotwire/Turbo configuration
+  # When using Devise with Hotwire/Turbo, the http status for error responses
+  # and some redirects must match the following. The default in Devise for existing
+  # apps is `200 OK` and `302 Found` respectively, but new apps are generated with
+  # these new defaults that match Hotwire/Turbo behavior.
+  # Note: These might become the new default in future versions of Devise.
+  config.responder.error_status = :unprocessable_content
+  config.responder.redirect_status = :see_other
 
   # ==> Configuration for :registerable
 
