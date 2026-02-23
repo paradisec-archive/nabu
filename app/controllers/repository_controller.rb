5db2cb1efe206d595d92d6ca15ddf17d55b5ba64
John Ferlito <johnf@inodes.org>
2026-02-24 10:12:42 +1100

    fix: use temporary redirects in repository controller
    Switch from 301 (moved_permanently) to 302 (found) to avoid browsers
    caching redirects that may change.

--- app/controllers/repository_controller.rb
+++ app/controllers/repository_controller.rb
@@ -7,9 +7,9 @@ class RepositoryController < ApplicationController
     raise ActionController::RoutingError, "Collection not found: #{params[:collection_identifier]}" if collection.nil?
 
     if params[:edit].present?
-      redirect_to edit_collection_url(collection), status: :moved_permanently
+      redirect_to edit_collection_url(collection), status: :found
     else
-      redirect_to helpers.oni_collection_url(collection), status: :moved_permanently, allow_other_host: true
+      redirect_to helpers.oni_collection_url(collection), status: :found, allow_other_host: true
     end
   end
 
@@ -23,9 +23,9 @@ class RepositoryController < ApplicationController
     raise ActionController::RoutingError, "Item not found: #{params[:collection_identifier]}-#{params[:collection_identifier]}" if item.nil?
 
     if params[:edit].present?
-      redirect_to edit_collection_item_url(collection, item), status: :moved_permanently
+      redirect_to edit_collection_item_url(collection, item), status: :found
     else
-      redirect_to helpers.oni_item_url(item), status: :moved_permanently, allow_other_host: true
+      redirect_to helpers.oni_item_url(item), status: :found, allow_other_host: true
     end
   end
 
