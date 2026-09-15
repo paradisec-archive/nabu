67eb00e94468abf89f2b5678fd68d3c273335171
John Ferlito <johnf@inodes.org>
2026-09-15 13:53:18 +1000

    feat: expose a language's source, dialect and equivalents over GraphQL
    The Language type gains source, dialect, source_uri and equivalents, each
    equivalent naming the other language of the pair and the evidence tags it
    rests on. archive_link now returns the source URI and is deprecated.
    
    Refs #1206

--- app/graphql/types/language_type.rb
+++ app/graphql/types/language_type.rb
@@ -1,13 +1,23 @@
 class Types::LanguageType < Types::BaseObject
-  field :archive_link, String, method: :language_archive_link, null: true, camelize: false
+  field :archive_link, String, method: :source_uri, null: true, camelize: false, deprecation_reason: 'Use source_uri'
   field :code, String, null: false
   field :countries, [Types::CountryType, null: true], null: true
+  field :dialect, Boolean, null: false
+  field :equivalents, [Types::LanguageEquivalentType], null: false
   field :id, ID, null: false
   field :items_for_content, [Types::ItemType, null: true], null: true, camelize: false
   field :items_for_language, [Types::ItemType, null: true], null: true, camelize: false
   field :name, String, null: false
   field :retired, Boolean, null: true
+  field :source, Types::LanguageSourceEnum, null: false
+  field :source_uri, String, null: true, camelize: false
 
   # TODO: this should be loaded through Item relationships instead
   field :collection, [Types::CollectionType, null: true], null: true
+
+  def equivalents
+    object.equivalents.includes(:language, :related_language).map do |pair|
+      { language: pair.other_than(object), evidence: pair.evidence }
+    end
+  end
 end
